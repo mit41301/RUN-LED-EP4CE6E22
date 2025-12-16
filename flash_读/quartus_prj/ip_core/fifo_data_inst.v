d5a3a7e5f61072bde6ad350158d136c5fc87e091
mit41301 <120297041+mit41301@users.noreply.github.com>
2025-12-16 19:02:10 +0530

    Delete flash_读 directory

--- flash_读/quartus_prj/ip_core/fifo_data_inst.v
+++
@@ -1,8 +0,0 @@
-fifo_data	fifo_data_inst (
-	.clock ( clock_sig ),
-	.data ( data_sig ),
-	.rdreq ( rdreq_sig ),
-	.wrreq ( wrreq_sig ),
-	.q ( q_sig ),
-	.usedw ( usedw_sig )
-	);
--- flash_读/sim/M25P16_VG_V12/fifo_sum.m
+++
@@ -1,7 +0,0 @@
-fid=fopen('data.txt','w+');
-for i =1:256
-    for j = 1:256
-        fprintf(fid,'%02x ',j-1);
-    end
-end
-fclose(fid);
